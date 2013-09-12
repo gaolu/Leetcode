4ea672ce569bf85726dc39013f4a9a6f38e1d160
Lu Gao <gaolu.adam@gmail.com>
2013-09-12 12:10:44 -0500

    Unique Binary Search Trees (Recursive)

---
+++ uniqueBinarySearchTrees_Iterative.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int numTrees(int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        // Catalan number count = (2n)! / ((n+1)! * n!)
+        int count = 1;
+        for(int i = 2; i <= n ; i++)
+            count = count * 2 * (2 * i - 1) / (i + 1);
+        return count;
+    }
+};
