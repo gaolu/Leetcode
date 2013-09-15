e0b1dcb8ee2c7c2fc414d956b77d5dbd5f3d51c5
Lu Gao <gaolu.adam@gmail.com>
2013-09-14 22:35:35 -0500

    Binary Tree Maximum Path Sum (Recursive)

---
+++ binaryTreeMaximumPathSum.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int maxPathSum(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        if(!root) return 0;
+        int currentSum;
+        int maxSum = INT_MIN;
+        helper(root, currentSum, maxSum);
+        return maxSum;
+    }
+    
+    void helper(TreeNode *root, int &currentSum, int &maxSum){
+        if(!root){
+            currentSum = 0;
+            return;
+        }
+        int leftSum, rightSum;
+        helper(root->left, leftSum, maxSum);
+        helper(root->right, rightSum, maxSum);
+        currentSum = max(root->val, max(root->val + leftSum, root->val + rightSum));
+        maxSum = max(maxSum, max(currentSum, root->val + leftSum + rightSum));
+    }
+};
