6ec5c7b5d7e2b489e88d2ebc4a0828e6a477b972
Lu Gao <gaolu.adam@gmail.com>
2013-09-20 00:02:12 -0500

    Jump Game

--- jumpGame.cpp
+++ jumpGame.cpp
@@ -1,9 +1,12 @@
-class Solution{
-	bool canJump(int[] A, int n){
-		int reach = 1;
-		for(int i = 0; i < n, i++){
-			reach = max(reach, A[i] + i + 1);
-		}
-		return reach >= n;
-	}
-}
+class Solution {
+public:
+    bool canJump(int A[], int n) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int maxReach = 1;
+        for(int i = 0; i < maxReach && maxReach < n; i++)
+        	// get the max distance that can be reach from each node
+            maxReach = max(maxReach, A[i] + (i + 1));
+        return maxReach >= n;
+    }
+};
